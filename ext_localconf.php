f969b571b1e2e99f7c1981310261572d79e6417b
DonChef660 <fehmi@raqipi.ch>
2017-11-29 10:52:06 +0100

    Added TypoScript configuration for additional HTML elements

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,9 @@
 <?php
-defined('TYPO3_MODE') or die();
+
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:T8_CK/Configuration/TSConfig/rte.txt">');
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['T8Custom'] = 'EXT:T8_CK/Configuration/RTE/Default.yaml';
+
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['T8Custom'] = 'EXT:T8_CK/Configuration/RTE/T8Custom.yaml';
